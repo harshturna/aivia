eca27cac457bfb94835a1a290e5de279c6111b0d
Harsh <74731135+harshturna@users.noreply.github.com>
2024-01-26 15:28:19 -0600

    fix features layout

--- components/landing/features.tsx
+++ components/landing/features.tsx
@@ -11,14 +11,14 @@ export default function Features() {
           {features.header}
         </HeadingText>
       ) : null}
-      <div className="grid grid-cols-1 gap-10">
+      <div className="grid grid-cols-1 gap-10 lg:mx-[20rem] md:mx-[10rem]">
         {features.content.map((cards: any) => {
           const Icon = cards.icon;
 
           return (
             <div
               key={cards.text}
-              className="flex flex-col gap-2 md:flex-row md:gap-8 md:text-left"
+              className="flex flex-col gap-2 md:flex-row md:gap-8 md:text-left sm:items-center sm:text-center xs:items-center"
             >
               <div className="flex">
                 <Icon className="h-[6rem] w-[6rem]" />
