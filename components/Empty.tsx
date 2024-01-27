f1390b4619ec7402b9d03f6e9458d118bac293c0
Harsh <74731135+harshturna@users.noreply.github.com>
2024-01-26 19:21:34 -0600

    Update Empty image

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -8,7 +8,7 @@ export const Empty = ({ label }: EmptyProps) => {
   return (
     <div className="h-full p-20 flex flex-col items-center">
       <div className="relative h-72 w-72">
-        <Image alt="Empty" fill src="/empty.png" />
+        <Image alt="Empty" fill src="https://res.cloudinary.com/dlp6wui7r/image/upload/v1706318362/aivia/empty.png" />
       </div>
       <p className="text-muted-foreground text-sm text-center capitalize">
         {label}
