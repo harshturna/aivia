96f74bbb047bef6a0d09d904e091e31ca5810d8b
Harsh <74731135+harshturna@users.noreply.github.com>
2024-02-04 05:19:05 -0600

    fix hero section padding

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -6,7 +6,7 @@ import { heroHeader } from "@/config/contents";
 
 export default function HeroHeader() {
   return (
-    <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-4 lg:py-20 mt-20">
+    <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-4 lg:py-5 mt-20">
       <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
         <div className="space-y-4">
           <h1 className="text-4xl font-bold lg:text-5xl">
