5dff2bfc5baccf2767d864750fd42cff65b4441c
Harsh <74731135+harshturna@users.noreply.github.com>
2024-03-19 18:43:30 -0500

    Update pricing.tsx

--- components/landing/pricing.tsx
+++ components/landing/pricing.tsx
@@ -53,7 +53,7 @@ export default function Pricing() {
             <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
               <div className="mx-auto max-w-xs px-8">
                 <p className="text-base font-semibold text-gray-600">
-                  Reccured monthly charged for unlimited use
+                  Reccured monthly for unlimited use
                 </p>
                 <p className="mt-6 flex items-baseline justify-center gap-x-2">
                   <span className="text-5xl font-bold tracking-tight text-gray-900">
