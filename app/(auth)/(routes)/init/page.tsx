8425503329f5705691c1ed2038c33783cf8d7b4f
Harsh <74731135+harshturna@users.noreply.github.com>
2025-01-27 04:18:06 -0600

    add env vars for guest user

--- app/(auth)/(routes)/init/page.tsx
+++ app/(auth)/(routes)/init/page.tsx
@@ -7,7 +7,7 @@ import { Loader } from "@/components/Loader";
 const Init = () => {
   useEffect(() => {
     router.refresh();
-    guestHandler(process.env.NEXT_PUBLIC_GUEST_EMAIL, process.env.NEXT_PUBLIC_GUEST_PASSWORD);
+    guestHandler(process.env.NEXT_PUBLIC_GUEST_EMAIL || "", process.env.NEXT_PUBLIC_GUEST_PASSWORD || "");
   }, []);
 
   const router = useRouter();
